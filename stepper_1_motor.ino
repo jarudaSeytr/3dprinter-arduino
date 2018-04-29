8c93e6bba0adbedb83d9cec82a8ccafd39b1f864
jarudaSeytr <38844727+jarudaSeytr@users.noreply.github.com>
2018-04-30 01:30:57 +0200

    Create stepper_1_motor.ino

---
+++ stepper_1_motor.ino
@@ -0,0 +1,62 @@
+#define IN1  11
+#define IN2  10
+#define IN3  9
+#define IN4  8
+
+
+int Paso [ 8 ][ 4 ] =
+{ {1, 0, 0, 0},
+  {1, 1, 0, 0},
+  {0, 1, 0, 0},
+  {0, 1, 1, 0},
+  {0, 0, 1, 0},
+  {0, 0, 1, 1},
+  {0, 0, 0, 1},
+  {1, 0, 0, 1}
+};
+
+int steps_left = 4095;
+boolean Direction = true;
+int Steps = 0; // Define el paso actual de la secuencia
+
+void setup()
+{
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+}
+
+void loop() {
+  
+  while(steps_left > 0) {
+    stepper();
+    steps_left--;
+    delay(1);
+  }
+  
+  delay(300);
+  Direction = ! Direction; // Invertimos la direceccion de giro
+  steps_left = 4095;
+}
+
+void stepper()
+{
+  digitalWrite(IN1, Paso[Steps][0]);
+  digitalWrite(IN2, Paso[Steps][1]);
+  digitalWrite(IN3, Paso[Steps][2]);
+  digitalWrite(IN4, Paso[Steps][3]);
+    
+  SetDirection();
+}
+
+void SetDirection() {
+  
+  if (Direction)
+    Steps++;
+  
+  else
+    Steps--;
+
+  Steps = (Steps + 8) % 8;
+}
